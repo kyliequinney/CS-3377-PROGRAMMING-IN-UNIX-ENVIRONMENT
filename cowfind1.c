2a49f89413dc2f085c490c3aa908b646a2368d6f
kyliequinney <94495097+kyliequinney@users.noreply.github.com>
2024-01-09 16:11:33 -0600

    Add files via upload

---
+++ cowfind1.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include <ctype.h>
+#include <string.h>
+
+#define MAXLEN 1000000
+char input[MAXLEN];
+
+int main() {
+        scanf("%s", input);
+
+        //CODE HERE!
+
+        char frontLeg = '(';
+        int backLegsCount = 0;
+        char backLeg = ')';
+
+        for (int i = 0; i < strlen(input); i++){
+                if (input[i] == frontLeg & input[i+1] == frontLeg){
+                        for (int j = i+2; j < strlen(input); j++){
+                                if (input[j] == backLeg & input[j+1] ==
+                                backLeg){
+                                        backLegsCount++;
+                                }
+                        }
+                }
+        }
+        printf("Total possible locations: %d", backLegsCount);
+        return 0;
+}
